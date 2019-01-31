dda07140fb09d9d416ef333103e5ae5ab863002e
zhonghaoliu <joshualiu94@gmail.com>
2019-01-31 11:35:21 -0500

    change greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: "there v2" });
 });
 
 const PORT = process.env.PORT || 5000;
