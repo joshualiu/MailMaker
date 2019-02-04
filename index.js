1917702f058a56655e47be67946e5e6c77c6380a
zhonghaoliu <joshualiu94@gmail.com>
2019-02-04 14:10:55 -0500

    setup sendgrind -- issue: email could not receive

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -26,6 +27,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express wil serve up production assets
---
+++ models/Recipient.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const RecipientSchema = new Schema({
+  email: String,
+  responded: { type: Boolean, default: false }
+});
+
+module.exports = RecipientSchema;
