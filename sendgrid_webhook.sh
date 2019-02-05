50b868c7caad66d3b9f59969d6c44893cde18de6
zhonghaoliu <joshualiu94@gmail.com>
2019-02-05 16:48:15 -0500

    finish handling webhook data

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,5 @@
 function localtunnel {
-  lt -s eogiroje-euwhowuweocciewoinviowvgiobn --port 5000
+  lt -s eogiroje-euwhowuweocc-jifwejoifwe232323-iewoinviowvgjiiobn --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
