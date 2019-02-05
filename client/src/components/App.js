593cf467c2559538615e283754686e778c9db199
zhonghaoliu <joshualiu94@gmail.com>
2019-02-05 11:30:11 -0500

    client side send surveys emails

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Header from "./Header";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
+import Header from "./Header";
 import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
+// import SurveyNew from "./SurveyNew";
 
 class App extends Component {
   componentDidMount() {
@@ -18,8 +21,8 @@ class App extends Component {
           <div>
             <Header />
             <Route exact path="/" component={Landing} />
-            {/* <Route exact path="/surveys" component={Dashboard}/> */}
-            {/* <Route exact path="/surveys/new" component={SurveyNew}/> */}
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route exact path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,16 @@
+// SurveyField contains logic to render a singler lable and text input
+import React from "react";
+
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} style={{ marginBottom: "5px" }} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
+    </div>
+  );
+};
+
+export default SurveyField;
