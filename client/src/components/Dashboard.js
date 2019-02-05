02ce84955bfb403962aa78176e5d49eec832d8a2
zhonghaoliu <joshualiu94@gmail.com>
2019-02-05 17:20:36 -0500

    showing users' survey results

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./surveys/SurveyList";
 
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+      <SurveyList />
       <div className="fixed-action-btn">
-        <Link to='/surveys/new' className="btn-floating btn-large red">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
         </Link>
       </div>
